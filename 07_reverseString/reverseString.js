4847a3b448babe33a4951147f673cd34f25b5a76
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-13 14:54:12 -0400

    Solve 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split('');
+    arr = arr.reverse();
+    let reversedStr = arr.join('');
+    return reversedStr;
 };
 
 // Do not edit below this line
