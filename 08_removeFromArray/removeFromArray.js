be0d6145a3943059922cefb22304eb10f2daed0c
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-13 17:36:23 -0400

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+/*Implement a function that takes an array and some other arguments then removes the other arguments from that array:
+
+let arr = ['cat', 'dog', 'tiger', 'wolf'];
+
+
+!! "stuff" is not handed to me as an array
+
+pseudocode:
+- for each "thing" I am handed after the initial arr, store it in a new arr2
+- use filter on the arr1 to get rid of any elements if they are contained in arr2
+
+
+*/
+
+const removeFromArray = function(arr, ...stuff) {
+    const result =  arr.filter((item) => !stuff.includes(item));
+    return result;
 };
 
 // Do not edit below this line
