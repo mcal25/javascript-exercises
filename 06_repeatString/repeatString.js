eea4b93e2b649b593356fac8a8e686982de682b5
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-13 14:51:29 -0400

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) return 'ERROR';
+    let val = '';
+    for (let i = 0; i < n; i++) {
+        val = val.concat(str);
+    }
+    return val;
 };
 
 // Do not edit below this line
